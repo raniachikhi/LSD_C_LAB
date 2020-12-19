f516ce1b8dcd0b5e3cfd5e71fc9240bf0f1c675e
raniachikhi <raniach0653@gmail~ail.com>
2020-12-19 02:13:23 +0100

    a comment about your changes

--- TP1/ex03/lu.c
+++ TP1/ex03/lu.c
@@ -4,19 +4,50 @@
 
 float *lu(float A[N][N], float B[N], int n)
 {
-
-  /******Implement the solution here******/
-  /***************************************/
-
-
-  /***************************************/
-  return (x);
+ float *X;
+ X=malloc(sizeof(float) * n);
+ float L[N][N],Y[N];
+ float S;
+ for(int k=0;k<n-1;k++)
+ {
+	for(int i=k+1;i<=n;i++)
+	{
+		L[i][k]=A[i][k]/A[k][k];
+		for(int j=k+1;j<n;j++)
+		{
+			A[i][j]=A[i][j]-L[i][k]*A[k][j];
+		}
+	}
+ }
+ Y[0]=B[0];
+ for(int i=1;i<n;i++)
+ {
+	S=0;
+	for(int j=0;j<i;j++)
+	{
+		S=S+L[i][j]*Y[j];
+	}
+	Y[i]=B[i]-S;
+ }
+ X[n-1]=Y[n-1]/A[n-1][n-1];
+ for(int i=n-2;i>=0;i--)
+ {
+	S=0;
+	for(int j=i+1;j<n;j++)
+	{
+		S=S+A[i][j] * X[j];
+	}
+	X[i]=(Y[i]-S)/A[i][i];
+ }
+	
+  
+  return (X);
 }
 
 int main()
 {
   float   A[N][N], B[N];
-  float   *x;
+  float   *X;
   int     n;
 
   printf("Enter the size of the matrix: ");
@@ -42,11 +73,11 @@ int main()
   }
 
   /* The calculation of the result */
-  x = lu(A, B, n);
+  X = lu(A, B, n);
 
   /* Printing the results */
   printf("\nThe resulting vector: [");
   for (int i = 0; i < n; i++)
-    printf("%f%c", x[i], ",]"[i == n - 1]);
+    printf("%f%c", X[i], ",]"[i == n - 1]);
 }
 
